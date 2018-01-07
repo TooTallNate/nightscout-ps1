6c96db0b968557890b6fea7c04c1d385d048bb32
Nathan Rajlich <nathan@tootallnate.net>
2018-01-06 17:51:32 -0800

    Fix `pack.sh` for rename

--- pack.sh
+++ pack.sh
@@ -7,9 +7,9 @@ tag="$(git describe --tags)"
 
 mkdir -p "${dir}"
 
-pkg bin/nightscout-ps1.js \
+pkg bin/nightscout-ps1-daemon.js \
   --config package.json \
-  --output "${dir}/nightscout-ps1-v${tag}" \
+  --output "${dir}/nightscout-ps1-daemon-v${tag}" \
   -t node9-alpine,node9-linux,node9-macos,node9-win
 
 for fullpath in "${dir}"/*; do
